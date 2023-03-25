9be7a3f0dfac049e27297e2685b2e9cf89e84083
thangmaiduc <thangmaiduc00@gmail.com>
2023-03-25 23:36:47 +0700

    fix error es

--- backend/config/es.js
+++ backend/config/es.js
@@ -1,9 +1,9 @@
 const elasticsearch = require('elasticsearch');
 
 const client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'info'
+  host: 'localhost:9200',
+  log: 'info',
+  apiVersion: '6.8',
 });
-
 
 module.exports = client;
